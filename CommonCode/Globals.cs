fd4ea025fc090cfce9a51fba296a9d554075d5fd
Ron Eaglin <ron.eaglin@gmail.com>
2022-02-04 15:37:43 -0500

    Removes XmlObjectDatabase - saved as separate file

--- CommonCode/Globals.cs
+++ CommonCode/Globals.cs
@@ -7,6 +7,7 @@ using WindowsAppBase.DomainCode;
 
 namespace WindowsAppBase
 {
+    // This should really be in the DomainCode directory as it is domain specific
     static class Globals
     {
         private static DomainCode.SampleProject project = new SampleProject();
